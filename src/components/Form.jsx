0f12737e1cef7356e3c9aa62140d6fa4dbf61144
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-26 23:47:46 -0300

    Add countries into form select

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -19,6 +19,13 @@ const Form = () => {
                     id="country"
                 >
                         <option value="">-- Select a country --</option>
+                        <option value="US">Estados Unidos</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="ES">España</option>
+                        <option value="PE">Perú</option>
 
                 </select>
                 <label htmlFor="country">Country: </label>
