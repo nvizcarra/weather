0d554ac2afbf1cb3043425fc9a6d0e1fe3187edc
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-26 23:54:54 -0300

    Form state, extraction and handleChange

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+
+    // Form state
+    const [search, saveSearch] = useState ({
+        city: '',
+        country: ''
+    })
+
+    // Extract city and country
+    const { city, country } = search;
+
+    // Function which puts elements into the state
+    const handleChange = e => {
+        // Update state
+        saveSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        });
+    }
+
     return (
 
         <form>
@@ -9,6 +28,8 @@ const Form = () => {
                     type="text"
                     name="city"
                     id="city"
+                    value={city}
+                    onChange={handleChange}
                 />
                 <label htmlFor="city">City: </label>
             </div>
@@ -17,6 +38,8 @@ const Form = () => {
                 <select
                     name="country"
                     id="country"
+                    value={country}
+                    onChange={handleChange}
                 >
                         <option value="">-- Select a country --</option>
                         <option value="US">Estados Unidos</option>
